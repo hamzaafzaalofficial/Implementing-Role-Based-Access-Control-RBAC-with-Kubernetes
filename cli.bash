366cdb6d5677ee371c352fa11e1095c859763970
Hamza Afzal <hamzaafzal.official@gmail.com>
2025-02-05 18:50:53 +0500

    Delete cli.bash

--- cli.bash
+++
@@ -1,7 +0,0 @@
-# Setup the Minikube, go to scripts/minikube setup in the repo
-# Setup the kubectl, go to scripts/kubectl setup in the repo
-# Setup the docker, go to scripts/docker setup in the repo
-# Start the minikube cluster
-minikube start --memory=6144 --cpus=4 --driver=docker --force
-
-
